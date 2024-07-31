90f11e34438ac1d3e4a62260e6e4813988d09fa1
Jimmy Lindsey <jimmlindsey@gmail.com>
2024-07-31 10:53:51 -0400

    Initial Commit

---
+++ Fitoku/FitokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitokuApp.swift
+//  Fitoku
+//
+//  Created by jimmy on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FitokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
